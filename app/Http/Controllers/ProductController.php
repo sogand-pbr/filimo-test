2747a00f65f13204a63baa8954136672cb45351b
sogand-pbr <sogandprh@gmail.com>
2024-05-29 21:39:10 +0330

    Perform the process with laravel cache

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 
 class ProductController extends Controller
@@ -14,19 +15,26 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $product = Product::with(['category']);
-
-        if (request()->has('min_price') && request()->has('max_price'))
-            $product = $product->whereBetween('final_price', [request('min_price') && request('max_price')]);
-        if (request('category')) {
-            $category = Category::firstWhere('title', request('category'));
-            $product = $product->where('category_id', $category->id);
-        }
-        if (request('search'))
-            $product = $product->where('title', "%" . request('search') . "%");
+        $cacheKey = 'products_' . md5(json_encode(request()->all()));
+        $data = Cache::remember($cacheKey, now()->addMinutes(10), function () {
+            $productQuery = Product::with(['category']);
+            if (request()->has('min_price') && request()->has('max_price')) {
+                $productQuery->whereBetween('final_price', [request('min_price'), request('max_price')]);
+            }
+            if (request('category')) {
+                $category = Category::firstWhere('title', request('category'));
+                if ($category) {
+                    $productQuery->where('category_id', $category->id);
+                }
+            }
+            if (request('search')) {
+                $productQuery->where('title', 'like', '%' . request('search') . '%');
+            }
+            return $productQuery->paginate(request('per_page', 18));
+        });
         return response()->json([
             'success' => true,
-            'data' => $product->paginate(request('per_page', '18')),
+            'data' => $data,
         ]);
     }
 
