96e4b9407c426e59176b3aedc4c918660f81611d
sogand-pbr <sogandprh@gmail.com>
2024-05-29 23:19:38 +0330

    The product image display problem has been resolved

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,11 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\Category;
-use App\Models\Product;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Storage;
+use App\Models\Category;
+use App\Models\Product;
 
 class ProductSeeder extends Seeder
 {
@@ -15,22 +14,28 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        $sourceFilePath = storage_path('app/public/1.jpg');
-        $extension = pathinfo($sourceFilePath, PATHINFO_EXTENSION);
+
+        $sourceFilePath = 'public/1.jpg';
+        if (!Storage::disk('local')->exists($sourceFilePath)) {
+            throw new \Exception("Source file does not exist at path: " . $sourceFilePath);
+        }
+        $extension = pathinfo(storage_path('app/public/1.jpg'), PATHINFO_EXTENSION);
         $randomize = rand(111111, 999999);
         $destinationDirectory = 'images/';
         $filename = $randomize . '.' . $extension;
         $destinationFilePath = $destinationDirectory . $filename;
-        Storage::disk('public')->move($sourceFilePath, $destinationFilePath);
+        Storage::disk('public')->put($destinationFilePath, Storage::disk('local')->get($sourceFilePath));
         $imageUrl = Storage::url($destinationFilePath);
-        $category = Category::all();
-        foreach ($category as $item) {
+        $categories = Category::all();
+
+        foreach ($categories as $category) {
             for ($i = 0; $i < rand(4, 8); $i++) {
                 Product::factory()->create([
-                    'category_id' => $item->id,
-                    'url' => url('/') . $imageUrl
+                    'category_id' => $category->id,
+                    'url' => $imageUrl,
                 ]);
             }
         }
     }
 }
+
